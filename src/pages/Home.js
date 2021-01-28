aaac05c608de3a070c1b941d9846ba24b65ec1aa
chuy <chuyalexgt@gmail.com>
2021-01-28 15:44:35 -0600

    Creacion de templates para la SPA

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+const Home = ()=>{
+    const view = `                                 //Esto es un template y es muy parecido a lo que se utiliza en react
+    <div class="Characters">
+        <article class="character-item">
+            <a href="#/1/">
+                <img src="image" alt="name">
+                <h2>name</h2>
+            </a>
+        </article>
+    </div>
+    `
+    return view
+}
+
+export default Home
